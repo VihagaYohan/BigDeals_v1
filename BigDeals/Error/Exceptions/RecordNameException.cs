c78d4ed2638a64aa2dca90232bfec5ff3f5f2faf
VihagaYohan <vihagayohan94@gmail.com>
2021-07-11 23:44:54 +0530

    Created custom exception to check whether category name starts with a number

---
+++ BigDeals/Error/Exceptions/RecordNameException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigDeals.Error.Exceptions
+{
+	public class RecordNameException:Exception
+	{
+		public string RecordName { get; set; }
+
+		public RecordNameException()
+		{
+
+		}
+
+		public RecordNameException(string message) : base(message)
+		{
+
+		}
+
+		public RecordNameException(string message, Exception inner) : base(message, inner)
+		{
+
+		}
+
+		public RecordNameException(string message, string recordName) : this(message)
+		{
+			RecordName = recordName;
+		}
+	}
+}
