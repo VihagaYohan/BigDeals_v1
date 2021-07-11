85d3dcb7ccb94ae2a5a1b057877a8053df4cccad
VihagaYohan <vihagayohan94@gmail.com>
2021-07-11 23:13:27 +0530

    Added add category functionality

--- BigDeals/Controllers/CategoriesController.cs
+++ BigDeals/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
 ﻿using BigDeals.Data.Models;
+using BigDeals.Data.Services;
+using BigDeals.Data.ViewModels.Category;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,12 +14,17 @@ namespace BigDeals.Controllers
 	[ApiController]
 	public class CategoriesController : ControllerBase
 	{
-
+		private readonly CategoryService _service;
+		public CategoriesController(CategoryService service)
+		{
+			_service = service;
+		}
 
 		[HttpPost]
-		public IActionResult AddCategories() 
+		public IActionResult AddCategories([FromBody]CategoryVM categoryVM) 
 		{
-			return Ok();
+			var result = _service.AddCategory(categoryVM);
+			return Ok(result);
 		}
 	}
 }
---
+++ BigDeals/Data/Services/CategoryService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using BigDeals.Data.Models;
+using BigDeals.Data.ViewModels.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigDeals.Data.Services
+{
+	public class CategoryService
+	{
+		private readonly AppDbContext _context;
+		private readonly IMapper _mapper;
+
+		public CategoryService(AppDbContext context,IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+
+		// add new product category
+		public Category AddCategory(CategoryVM category) 
+		{
+			var productCategory = _mapper.Map<Category>(category); 
+			 _context.Categories.Add(productCategory);
+			var result = _context.SaveChanges();
+
+			if (productCategory == null) 
+			{
+				return null;
+			}
+			
+			return productCategory;
+		}
+	}
+}
